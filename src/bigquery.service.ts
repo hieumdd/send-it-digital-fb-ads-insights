879d6bc21b71d794f01b9a9c395ccb1bd42e621c
HM <hieumdd@gmail.com>
2023-09-03 11:45:35 +0700

    Revert "debug: Change dataset"
    This reverts commit 931a39b5c540369648df7ab74c5d8c0ec05d7fb4.

--- src/bigquery.service.ts
+++ src/bigquery.service.ts
@@ -2,7 +2,7 @@ import { BigQuery } from '@google-cloud/bigquery';
 
 const client = new BigQuery();
 
-const DATASET = 'Facebook_dev';
+const DATASET = 'Facebook';
 
 type CreateLoadStreamOptions = {
     table: string;
