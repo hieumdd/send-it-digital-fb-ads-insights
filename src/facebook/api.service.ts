fcd97cb38d476b0f95513ace7c1906a317522cfb
HM <hieumdd@gmail.com>
2024-03-17 21:54:14 +0700

    feat: Add dynamic api version

--- src/facebook/api.service.ts
+++ src/facebook/api.service.ts
@@ -1,17 +1,22 @@
 import axios from 'axios';
 import qs from 'query-string';
 
-export const getClient = async () => {
-    const accessToken = await axios
+const getSecret = async (name: string) => {
+    return await axios
         .request<{ value: { raw: string } }>({
             method: 'GET',
             url: 'https://api.doppler.com/v3/configs/config/secret',
-            params: { project: 'facebook', config: 'master', name: 'USER_ACCESS_TOKEN' },
+            params: { project: 'facebook', config: 'master', name },
             auth: { username: process.env.DOPPLER_TOKEN ?? '', password: '' },
         })
         .then(({ data }) => data.value.raw);
+};
 
-    const apiVersion = 'v19.0';
+export const getClient = async () => {
+    const [apiVersion, accessToken] = await Promise.all([
+        getSecret('API_VERSION'),
+        getSecret('USER_ACCESS_TOKEN'),
+    ]);
 
     return axios.create({
         baseURL: `https://graph.facebook.com/${apiVersion}`,
